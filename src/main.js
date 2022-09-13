bf97867a95801c4163de80a39ca29d8b46d81c2a
Mark Mckinney <mmckinneyjr@gmail.com>
2022-09-13 08:47:07 -0500

    config

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-
 import axios from "axios";
-process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://capstone-pool-app.herokuapp.com/";
+
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://capstone-pool-app.herokuapp.com/";
 
 createApp(App).use(router).mount("#app");
 
